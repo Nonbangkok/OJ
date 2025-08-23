63b2f94716e43282b61d42680225c2247bcec818
YouBothReverse <nonbangkokgamer@gmail.com>
2025-08-23 23:57:37 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'http://localhost:3001',
+  origin: process.env.CORS_ORIGIN || 'http://localhost:3001',
   credentials: true
 }));
 app.use(express.json());
