b34d3545995a4430492007e410534bb8c76deb90
YouBothReverse <nonbangkokgamer@gmail.com>
2025-08-24 23:51:54 +0700

    Update to Docker

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,9 @@
-require('dotenv').config();
 const { Pool } = require('pg');
 
+// The DATABASE_URL environment variable will be injected by Docker Compose.
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = {
