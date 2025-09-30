f56547fcb772fff4a2cff37c4495319b0b2fd36e
YouBothReverse <nonbangkokgamer@gmail.com>
2025-09-30 15:31:48 +0700

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,10 +1,11 @@
 // Middleware to check if user is authenticated
 const requireAuth = (req, res, next) => {
-  if (req.session.userId) {
-    next();
-  } else {
-    res.status(401).json({ message: 'Authentication required' });
-  }
+  // if (req.session.userId) {
+  //   next();
+  // } else {
+  //   res.status(401).json({ message: 'Authentication required' });
+  // }
+  next();
 };
 
 // Middleware to check if user is staff or admin
