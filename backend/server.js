ae03f44d2e1e2bac45252ae698db4e10e1cb23d4
YouBothReverse <nonbangkokgamer@gmail.com>
2025-09-30 16:01:12 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ app.use(express.json());
 // Configure CORS using the cors middleware
 app.use(cors({
   origin: 'https://www.woi-grader.com',
-  // credentials: true,
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   exposedHeaders: ['Set-Cookie'],
