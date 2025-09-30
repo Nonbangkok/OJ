b589ac9224b7695d641b0c541a459b4bc4c1032b
YouBothReverse <nonbangkokgamer@gmail.com>
2025-09-30 14:16:58 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://www.woi-grader.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   
   // Handle preflight OPTIONS requests
