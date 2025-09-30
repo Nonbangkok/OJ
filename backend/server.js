e6ce3364f983f72765331e329881032fb703155d
YouBothReverse <nonbangkokgamer@gmail.com>
2025-09-30 14:32:15 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -47,10 +47,11 @@ const port = process.env.PORT;
 app.use(express.json());
 
 // Add a middleware to set the CORS header
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.woi-grader.com');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Add other allowed methods
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Add other allowed headers
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
