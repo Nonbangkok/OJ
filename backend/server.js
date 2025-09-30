393fbd150f06f87a45034e501c9dc49879971ed3
YouBothReverse <nonbangkokgamer@gmail.com>
2025-09-30 14:39:49 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -46,16 +46,18 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-// Add a middleware to set the CORS header
-// app.use(function (req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   next();
-// });
-
-app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.woi-grader.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  
+  // Handle preflight OPTIONS requests
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204); // No content, just send headers
+  }
+  next();
+});
 
 // PostgreSQL session store setup
 app.use(session({
